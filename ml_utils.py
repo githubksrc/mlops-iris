516b0a20b619cd6ebf15c51a1c138dad25699d54
ksrc_murthy <ksrc_murthy@rediffmail.com>
2021-07-11 12:20:20 +0530

    added 2 more test cases

--- ml_utils.py
+++ ml_utils.py
@@ -52,3 +52,4 @@ def retrain(data):
     # fit the classifier again based on the new data obtained
     clf.fit(X, y)  
     knnmodel.fit(X,y) #Murthy
+    
--- test_app.py
+++ test_app.py
@@ -14,13 +14,44 @@ def test_ping():
 def test_pred_virginica():
     # defining a sample payload for the testcase
     payload = {
-        "sepal_length": 3,
-        "sepal_width": 5,
-        "petal_length": 3.2,
-        "petal_width": 4.4,
+        "sepal_length": 7.2,
+        "sepal_width": 3.2,
+        "petal_length": 6.3,
+        "petal_width": 3.1,
     }
     with TestClient(app) as client:
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+
+# test to check if Iris Versicolour is classified correctly 
+def test_pred_Versicolour():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 4.2,
+        "sepal_width": 2.2,
+        "petal_length": 3.1,
+        "petal_width": 2.1,
+
+        }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour"}
+
+def test_pred_Setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+        }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
